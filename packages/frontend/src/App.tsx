7e39e44d5c8f98b373b50b5b9b1d657bb17914b0
Bálint Berente <balint.berente@gmail.com>
2021-12-21 23:33:47 +0100

    Scroll to top and default routing

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -8,17 +8,21 @@ import {StopPage} from "./components/pages/stop-page";
 import {Navbar} from "./components/elements/navbar";
 import {TrainPage} from "./components/pages/train-page";
 import {Footer} from "./components/elements/footer";
+import {ErrorPage} from "./components/pages/error";
+import ScrollToTop from "./utils/scroll-to-top";
 
 function App() {
   return (
     <Body>
       <BrowserRouter>
+          <ScrollToTop/>
           <Navbar/>
           <Routes>
-            <Route path="/train/:id" element={<TrainPage/>} />
-            <Route path="/stops/:id" element={<StopPage/>} />
-          <Route path="/stops" element={<StopsPage/>}/>
-          <Route path="/" element={<StopsPage/>} />
+              <Route path="/stops/:id" element={<StopPage/>}/>
+              <Route path="/stops" element={<StopsPage/>}/>
+              <Route path="train/:id" element={<TrainPage/>} />
+              <Route path="/" element={<StopsPage/>}/>
+              <Route path="*" element={<ErrorPage text="A keresett oldal nem található!"/>}/>
         </Routes>
           <Footer/>
       </BrowserRouter>
